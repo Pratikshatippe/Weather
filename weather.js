d9e40bf9e1826582ea60420066b56f258cc97592
Pratikshatippe <54614091+Pratikshatippe@users.noreply.github.com>
2019-08-30 11:47:08 +0530

    Text Display

--- weather.js
+++ weather.js
@@ -2,20 +2,20 @@
 // Displays data on webpage
 function showInput() {
   var information = [
-    { cityname: "Kolhapur, Maharashtra", type: "humidity ", temperature: 30 },
-    { cityname: "Pune, Maharashtra" , type: "Mostly cloudy", temperature: 28},
-    { cityname: "Delhi, Maharashtra", type: "wind", temperature: 24},
+    { cityname: "Pune, Maharashtra", type: "humidity ", temperature: 30 },
+    { cityname: "Delhi, Maharashtra" , type: "Mostly cloudy", temperature: 28},
+    { cityname: "Kolhapur, Maharashtra", type: "wind", temperature: 24},
     { cityname: "Bengaluru, Karnataka", type: "Partly Cloudy", temperature: 21}
 ]
   var e = document.getElementById("list");
-  var strUser = e.options[e.selectedIndex].value;
-  var x = information.find(v => v.cityname == strUser);
+  var currentCity = e.options[e.selectedIndex].value;
+  var x = information.find(v => v.cityname == currentCity);
 
 document.getElementById("demo").innerHTML=x['cityname'];
 document.getElementById("demo3").innerHTML = x['type'];
 document.getElementById("demo4").innerHTML = x['temperature'];
-                
-}
+  }
+  
 // // Date and Time
 var today = new Date();
 var day = today.getDay();
