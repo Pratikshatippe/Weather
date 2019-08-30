1cb77f933a8148b11d2e8c89201bceb31e301191
Pratikshatippe <54614091+Pratikshatippe@users.noreply.github.com>
2019-08-30 18:55:52 +0530

    Temperature part is done

--- weather.js
+++ weather.js
@@ -1,21 +1,23 @@
 
+
+var information = [
+  { cityname: "Pune, Maharashtra", type: "Rainy ☔", temperature: 40 },
+  { cityname: "Delhi, Maharashtra" , type: "Mostly cloudy ☁", temperature: 28},
+  { cityname: "Kolhapur, Maharashtra", type: "sunny ☀️", temperature: 24},
+  { cityname: "Bengaluru, Karnataka", type: "Partly Cloudy ⛅", temperature: 21}
+] 
 // Displays data on webpage
 function showInput() {
-  var information = [
-    { cityname: "Pune, Maharashtra", type: "humidity ", temperature: 30 },
-    { cityname: "Delhi, Maharashtra" , type: "Mostly cloudy", temperature: 28},
-    { cityname: "Kolhapur, Maharashtra", type: "wind", temperature: 24},
-    { cityname: "Bengaluru, Karnataka", type: "Partly Cloudy", temperature: 21}
-]
+  
   var e = document.getElementById("list");
   var currentCity = e.options[e.selectedIndex].value;
-  var x = information.find(v => v.cityname == currentCity);
+  var x = information.filter(v => v.cityname == currentCity);
 
-document.getElementById("demo").innerHTML=x['cityname'];
-document.getElementById("demo3").innerHTML = x['type'];
-document.getElementById("demo4").innerHTML = x['temperature'];
-  }
-  
+document.getElementById("demo").innerHTML=x[0].cityname;
+document.getElementById("demo3").innerHTML = x[0]['type'];
+document.getElementById("demo4").innerHTML = x[0]['temperature'];
+} 
+
 // // Date and Time
 var today = new Date();
 var day = today.getDay();
@@ -26,4 +28,31 @@ today.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true
 )
 document.getElementById("demo2").innerHTML = time1;
 
-// temperature
+// // temperature
+function fahrenheit()
+{
+  var e = document.getElementById("list");
+  var currentCity = e.options[e.selectedIndex].value;
+  var x1 = information.filter(v => v.cityname == currentCity);
+    // var currentCity = e.options[e.selectedIndex].value;
+
+  
+// console.log(fahrenheit);
+  var fahrenheit;
+  if(fahrenheit != ''){
+    fahrenheit = (x1[0].temperature * 9/5) + 32; 
+    document.getElementById("demo4").innerHTML = fahrenheit;
+  }
+}
+
+// function celsius()
+// {
+//   var fahrenheit = document.getElementById("demo4").innerHTML;
+// // console.log(fahrenheit);
+//   var f;
+//   if(fahrenheit != ''){
+//     f = (fahrenheit - 32) * 5/9; 
+//     document.getElementById("demo4").innerHTML = f;
+//   }
+// }
+  
