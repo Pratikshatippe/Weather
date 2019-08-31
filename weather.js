3790d6bec7df755da8f4cc86293b0834cf2984e7
Pratikshatippe <54614091+Pratikshatippe@users.noreply.github.com>
2019-08-31 11:24:41 +0530

    Update file

--- weather.js
+++ weather.js
@@ -1,20 +1,21 @@
 
 
 var information = [
-  { cityname: "Pune, Maharashtra", type: "Rainy ☔", temperature: 25 },
-  { cityname: "Chennai, Tamil Nadu" , type: "Mostly cloudy ☁", temperature: 28},
-  { cityname: "Kolhapur, Maharashtra", type: "sunny ☀️", temperature: 32},
-  { cityname: "Bengaluru, Karnataka", type: "Partly Cloudy ⛅", temperature: 21},
-  { cityname: "Lucknow, Uttar Pradesh", type: "sunny ☀️", temperature: 34},
-  { cityname: "Bhopal, Madhya Pradesh", type: "Mostly Cloudy ☁", temperature: 22}
+  { cityname: "Pune", State: "Maharashtra", type: "Rainy ☔", temperature: 25 },
+  { cityname: "Chennai", State: "Tamil Nadu", type: "Mostly cloudy ☁", temperature: 28},
+  { cityname: "Kolhapu", State: "Maharashtra", type: "sunny ☀️", temperature: 32},
+  { cityname: "Bengaluru", State: "Karnataka", type: "Partly Cloudy ⛅", temperature: 21},
+  { cityname: "Lucknow", State: "Uttar Pradesh", type: "sunny ☀️", temperature: 34},
+  { cityname: "Bhopal", State: "Madhya Pradesh", type: "Mostly Cloudy ☁", temperature: 22}
 ] 
 //compare value with array and displays data on webpage
 function showInput() {
-  var e = document.getElementById("list");
-  var currentCity = e.options[e.selectedIndex].value;
-  var x = information.find(v => v.cityname == currentCity);
+  var e = document.getElementById("list").value;
+  // var currentCity = e.options[e.selectedIndex].value;
+  var x = information.find(v => v.cityname == e);
 
   document.getElementById("demo").innerHTML=x.cityname;
+  document.getElementById("st").innerHTML=x.State;
   document.getElementById("demo3").innerHTML = x.type;
   document.getElementById("demo4").innerHTML = x.temperature;
 } 
@@ -32,13 +33,13 @@ document.getElementById("demo2").innerHTML = time1;
 // // Celsius to fahrenheit conversion of temperature
 function fahrenheit()
 {
-  var e = document.getElementById("list");
-  var currentCity = e.options[e.selectedIndex].value;
-  var x1 = information.find(v => v.cityname == currentCity);
+  var e = document.getElementById("list").value;
+  // var currentCity = e.options[e.selectedIndex].value;
+  var x1 = information.find(v => v.cityname == e);
  // console.log(fahrenheit);
   var fahrenheit;
   // if(fahrenheit != ''){
-    fahrenheit = (x1.temperature * 9/5) + 32; 
+    fahrenheit = Math.round((x1.temperature * 9/5) + 32); 
     document.getElementById("demo4").innerHTML = fahrenheit;
   // }
 }
