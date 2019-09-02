46fadc39cbcf5109adf37efc443e49f769045f42
Pratikshatippe <54614091+Pratikshatippe@users.noreply.github.com>
2019-09-02 12:19:04 +0530

    Updated file

--- weather.js
+++ weather.js
@@ -3,7 +3,7 @@
 var information = [
   { cityname: "Pune", State: "Maharashtra", type: "Rainy ☔", temperature: 25 },
   { cityname: "Chennai", State: "Tamil Nadu", type: "Mostly cloudy ☁", temperature: 28},
-  { cityname: "Kolhapu", State: "Maharashtra", type: "sunny ☀️", temperature: 32},
+  { cityname: "Kolhapur", State: "Maharashtra", type: "sunny ☀️", temperature: 32},
   { cityname: "Bengaluru", State: "Karnataka", type: "Partly Cloudy ⛅", temperature: 21},
   { cityname: "Lucknow", State: "Uttar Pradesh", type: "sunny ☀️", temperature: 34},
   { cityname: "Bhopal", State: "Madhya Pradesh", type: "Mostly Cloudy ☁", temperature: 22}
@@ -14,21 +14,21 @@ function showInput() {
   // var currentCity = e.options[e.selectedIndex].value;
   var x = information.find(v => v.cityname == e);
 
-  document.getElementById("demo").innerHTML=x.cityname;
-  document.getElementById("st").innerHTML=x.State;
-  document.getElementById("demo3").innerHTML = x.type;
-  document.getElementById("demo4").innerHTML = x.temperature;
+  document.getElementById("city").innerHTML=x.cityname;
+  document.getElementById("state").innerHTML=x.State;
+  document.getElementById("weather").innerHTML = x.type;
+  document.getElementById("temp").innerHTML = x.temperature;
 } 
 
 // // Date and Time
 var today = new Date();
 var day = today.getDay();
 var daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
-document.getElementById("demo1").innerHTML = daylist[day];
+document.getElementById("day").innerHTML = daylist[day];
 var time1 = (
 today.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
 )
-document.getElementById("demo2").innerHTML = time1;
+document.getElementById("time").innerHTML = time1;
 
 // // Celsius to fahrenheit conversion of temperature
 function fahrenheit()
@@ -40,7 +40,7 @@ function fahrenheit()
   var fahrenheit;
   // if(fahrenheit != ''){
     fahrenheit = Math.round((x1.temperature * 9/5) + 32); 
-    document.getElementById("demo4").innerHTML = fahrenheit;
+    document.getElementById("temp").innerHTML = fahrenheit;
   // }
 }
 
