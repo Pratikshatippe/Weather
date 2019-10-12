25926abe5268c5bc6f95f1cebe385135c836b727
Pratikshatippe <54614091+Pratikshatippe@users.noreply.github.com>
2019-10-12 09:46:34 +0530

    using getJSON

--- fun.js
+++ fun.js
@@ -16,9 +16,8 @@ document.getElementById("time").innerHTML = daylist[day]+","+time;
 getApiData(){
 document.getElementById("search").onclick = () =>{
  let select = document.getElementById("list").value;
- fetch(`http://api.openweathermap.org/data/2.5/weather?q=${select}&units=metric&APPID=5299c506b1cefa6863651e1dff0b3cc8`).then(data =>{
- return data.json();
- }).then(data1 =>{
+ $.getJSON("http://api.openweathermap.org/data/2.5/weather?q="+select+"&units=metric&APPID=5299c506b1cefa6863651e1dff0b3cc8",
+ function(data1){
   //  console.log(data1);
 
    document.getElementById("city").innerHTML=data1.name;
